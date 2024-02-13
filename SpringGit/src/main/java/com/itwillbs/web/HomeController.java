67b5995c729e02ff3a5a7e49778ed4027bf4adfc
유지인 <jiin4083@naver.com>
2024-02-13 15:40:19 +0900

    아이티윌 - 유지인 집에서 Test2 작성 완료

--- SpringGit/src/main/java/com/itwillbs/web/HomeController.java
+++ SpringGit/src/main/java/com/itwillbs/web/HomeController.java
@@ -38,9 +38,17 @@ public class HomeController {
 	
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public void Test() {
-		logger.debug("Test() 호출");
-		logger.debug("1. 서비스 객체 생성");
-		logger.debug("2. DAO 객체 생성");
-		logger.debug("3. mapper 호출");
+		logger.debug("Test() �샇異�");
+		logger.debug("1. �꽌鍮꾩뒪 媛앹껜 �깮�꽦");
+		logger.debug("2. DAO 媛앹껜 �깮�꽦");
+		logger.debug("3. mapper �샇異�");
+	}
+	
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public void Test2() {
+		logger.debug("Test() �샇異�");
+		logger.debug("1. �꽌鍮꾩뒪 媛앹껜 �깮�꽦");
+		logger.debug("2. DAO 媛앹껜 �깮�꽦");
+		logger.debug("3. mapper �샇異�");
 	}
 }
