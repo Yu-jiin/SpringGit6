f50e1f063821f7e7887d87ab1e670bcee16cd51e
유지인 <jiin4083@naver.com>
2024-04-02 16:33:48 +0900

    restController

--- SpringREST/src/main/java/com/itwillbs/controller/SampleRestController1.java
+++ SpringREST/src/main/java/com/itwillbs/controller/SampleRestController1.java
@@ -1,5 +1,6 @@
 package com.itwillbs.controller;
 
+import java.net.http.HttpHeaders;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
@@ -7,8 +8,14 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -88,6 +95,126 @@ public class SampleRestController1 {
 		}
 		return memberList;
 	}
+	
+	
+	// http://localhost:8088/test7/1000
+	// http://localhost:8088/test7/555
+	// http://localhost:8088/test7/itwill
+	// http://localhost:8088/1000
+	// http://localhost:8088/test7
+	@RequestMapping(value = "/test7/{bno}", method = RequestMethod.GET)
+	public int test7(@PathVariable("bno") int bno) throws Exception{
+		logger.info(" test7 실행 ");
+		
+		logger.info(" bno : " + bno);
+		return 0;
+	}
+	
+	
+	// http://localhost:8088/test/add
+	// ajax 방식으로 전달된 데이터 저장
+	@RequestMapping(value = "/test/add", method = RequestMethod.POST)
+	public void testADD(@RequestBody MemberVO vo) throws Exception{
+		// @RequestBody : JSON 타입 문자열 데이터를 객체의 형태로 형변환 
+		logger.info(" testADD() 실행 ");
+		logger.info("vo >> " + vo);
+		// 서비스 객체 주입 -> 메서드 호출 -> 결과 리턴 -> view 페이지 전달
+		
+		
+	}
+	
+	
+	// ResponseEntity 클래스 : @RestController 에서 별도의 view페이지가 없는 데이터를 처리할 때
+	// 						   발생하는 예외상황을 처리하기위한 클래스
+	
+	// HTTP 상태코드 (status)
+	// 100번대 : 정보응답 / 현재 데이터의 처리중인 상태
+	//	-100 : 데이터의 일부를 서버가 받은 상태 
+	
+	// 200번대 : 성공응답 / 정상적인 응답 
+	// 	-200 : 에러없이 정상처리
+	// 	-201 : 요청의 처리가 성공적, 그 결과로 새로운 리소스가 생성됨
+	//	-204 : 정상처리 되었으나, 서버에서 보내줄 데이터가 없음 
+	
+	// 300번대 : 리다이렉션 메세지 / 다른 URL 처리상태
+	//	-301 : 요청된 페이지가 새 URL으로 변경 
+	//	-304 : 이미 기존의 데이터정보와 변경이 없음
+	
+	// 400번대 : 클라이언트 오류응답 / 서버에서 인식이 불가능
+	//	-400 : 전송한 데이터에 문제가 있어서 서버가 인지불가 (ajax)
+	//	-401 : 인증되지 않음 
+	//	-403 : 서버에서 허락 X
+	//	-404 : URL에 해당하는 리소스를 찾을 수 없음
+	//	-405/406 : 전송 방식에 따른 접근 허용 여부 (REST)
+	
+	// 500번대 : 서버 오류응답 / 서버 내부의 문제
+	//	-500 : 서버 내부에서 문제가 발생 (컴파일 에러/ 실행 에러)
+	// 	-502 : 게이트웨이/프록시 상태 문제 (과부하)
+	//	-503 : 서버가 요청을 처리할 상태 X (일시적 과부하/서비스 중단)
+	
+	
+	// => ResponseEntity 객체가 처리 결과의 데이터 + HTTP 상태코드 전달 
+	
+	/*
+	 * // http://localhost:8088/test8
+	 * 
+	 * @RequestMapping(value = "/test8", method = RequestMethod.GET) public String
+	 * test8() throws Exception{ logger.info(" test8() 호출 ");
+	 * 
+	 * 
+	 * return "I want to go home"; // 처리되는 데이터만 확인, 상태확인 불가능
+	 * 
+	 * }
+	 */
+	
+	// http://localhost:8088/test8	
+	@RequestMapping(value = "/test8", method = RequestMethod.GET)
+	public ResponseEntity<Void> test8() throws Exception{
+		logger.info(" test8() 호출 ");
+		
+		
+		// return "I want to go home"; // 처리되는 데이터만 확인, 상태확인 불가능
+		return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+	}
+	
+	// http://localhost:8088/test9	
+	@RequestMapping(value = "/test9", method = RequestMethod.GET)
+	public ResponseEntity<List<MemberVO>> test9() throws Exception{
+		logger.info(" test9() 호출 ");
+		List<MemberVO> memberList = new ArrayList<MemberVO>();
+		
+		MemberVO vo = null;
+		for(int i =0; i<10; i++) {
+			vo = new MemberVO();
+			vo.setUserid("home"+i);
+			vo.setUserpw("1234"+i);
+			vo.setUsername("go home"+i);
+			vo.setUseremail("plz@gohome.com"+i);			
+			
+			memberList.add(vo);
+		}
+
+		return new ResponseEntity<List<MemberVO>>(memberList,HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	// http://localhost:8088/test10
+	@RequestMapping(value = "/test10", method = RequestMethod.GET)
+	public ResponseEntity test10() throws Exception{
+		logger.info(" test10() 호출 ");
+		org.springframework.http.HttpHeaders respHeaders = new org.springframework.http.HttpHeaders();
+		respHeaders.add("Content-Type", "text/html; charset=utf-8");
+	
+		String result = "<script>";
+		result += " alert('test message'); ";
+		result += " location.href='http://www.naver.com'; ";
+		result += "</script>";
+		
+		return new ResponseEntity(result,respHeaders,HttpStatus.OK);
+	}
+	
+	
+	
+	
 	
 	
 	
--- SpringREST/src/main/webapp/WEB-INF/views/home.jsp
+++ SpringREST/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,8 +1,38 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ page session="false" contentType="text/html; charset=UTF-8"%>
 <html>
 <head>
 	<title>Home</title>
+	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			// 버튼 클릭시, 데이터 생성 + 전송 
+			$("#btnSend").click(function(){
+				alert("버튼 클릭 !");
+				
+				//객체 생성
+				var member = {
+					"userid":"ITWILL",
+					"username":"아이티윌",
+					"userpw":"12345",
+					"useremail":"itwill@itwill.com"
+				};
+				// console.log(member);
+				// ajax를 사용한 정보 전달
+				$.ajax({
+					type:"post",
+					url:"/test/add",
+					contentType:"application/json",
+					data: JSON.stringify(member), // JSON객체 -> JSON문자열 변환
+					success:function(){
+						alert(" /test/add 페이지 다녀옴 ! ");
+					}
+				});
+				
+			});// click
+			
+		});
+	</script>
 </head>
 <body>
 <h1>
@@ -10,5 +40,20 @@
 </h1>
 
 <P>  The time on the server is ${serverTime}. </P>
+
+<input id="btnSend" type="button" value="회원정보 전송하기">
+
+
+
+
+
+
+
+
+
+
+
+
+
 </body>
 </html>
